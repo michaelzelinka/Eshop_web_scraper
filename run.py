0dd5b6858e33aaf632adb2c220c3ca5aef3af289
Michael Zelinka <59862231+michaelzelinka@users.noreply.github.com>
2026-04-06 13:32:04 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from scraper import scrape_all
 from utilities import save_csv, upload_to_sheets
 from send_alert import send_discord_alert   # ✅ přidáno
 
-SHEET_ID = "TVŮJ_SHEET_ID"   # vyplníš později nebo necháš prázdné
+SHEET_ID = "1A1tylaob6it1_BKXqTkbwv_Duqm6rQrSqYU9FEnAHf8"   
 
 if __name__ == "__main__":
     print("▶ Running scraper...")
